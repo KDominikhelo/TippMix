8b53470a084d30e7c41d0126b184c331eecd11b7
martinkovacs22 <79226892+martinkovacs22@users.noreply.github.com>
2023-03-02 21:00:33 +0100

    MakeDB
    Létrehoztam a projectet

--- TippMix/src/BD/Db_Connect.java
+++ TippMix/src/BD/Db_Connect.java
@@ -1,21 +1,37 @@
 
 package BD;
 
+import moduls.Player;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Db_Connect {
 
 
     private Connection conn;
+    
+    
+    
 
-
+    private static ArrayList<Player> player = new ArrayList<>();
     private String url = "jdbc:mysql://localhost/tippmix";
 
 
     private String user = "root";
     private String password = "";
 
-    public Db_Connect() {
+    private static Db_Connect inc = null;
+    
+    public static Db_Connect getInc(){
+        
+        if(inc == null){
+            inc = new Db_Connect();
+        }
+        
+        return inc;
+    }
+    
+    private Db_Connect() {
 
         try {
 
@@ -32,6 +48,28 @@ public class Db_Connect {
     }
 
 
+    public ArrayList<Player> getPlayer() throws SQLException{
+        Connection conn = getConnection();
+        ArrayList<Player> pl = new ArrayList<>();
+        String storedProcedureCall = "{call getPlayer()}";
+            CallableStatement callableStatement = conn.prepareCall(storedProcedureCall);
+            //callableStatement.setInt(1, 123); // beállítjuk az első paraméter értékét
+            //callableStatement.setString(2, "John"); // beállítjuk a második paraméter értékét
+            //callableStatement.setDouble(3, 45.67); // beállítjuk a harmadik paraméter értékét
+            callableStatement.execute();
+            ResultSet resultSet = callableStatement.getResultSet();
+            while (resultSet.next()) {
+                String result = resultSet.getString("name");
+                // eredmény feldolgozása
+                Player p = new Player(result);
+                pl.add(p);
+            }
+
+            // Objektumok bezárása
+            resultSet.close();
+            callableStatement.close();
+        return pl;
+    }
     public void closeConnection() {
         try {
             conn.close();
--- TippMix/src/tippmix/TippMix.java
+++ TippMix/src/tippmix/TippMix.java
@@ -1,11 +1,22 @@
 
 package tippmix;
 
+import BD.Db_Connect;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import moduls.Player;
+
 
 public class TippMix {
 
-    public static void main(String[] args) {
-            
+    public static void main(String[] args) throws SQLException {
+          // System.out.println(Db_Connect.getInc().getConnection());
+          
+          ArrayList<Player> pl = Db_Connect.getInc().getPlayer();
+          for (Player player : pl) {
+              System.out.println(player.name);
+        }
+          
     }
     
 }
